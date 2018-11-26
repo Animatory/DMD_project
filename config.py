0fb2a1604d19928f48c4b9a8d0c58ad6bc18e961
RedMoon32 <rinat.babichev@gmail.com>
2018-11-26 21:06:31 +0300

    Merge branch 'master' of https://github.com/Animatory/DMD_project
    # Conflicts:
    #	sql/fill_data.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-database_host = 'localhost'
+database_host = 'db'
 database_port = 5432
-database_user = 'inno_points'
-database_password = 'inno_points'
-database = 'inno_points'
+database_user = 'postgres'
+database_password = 'car_sharing_password'
+database = 'car_sharing'
 start_stamp = 154e7  # 2018 /11 / 26
 end_stamp = 1543190400
