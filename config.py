0da5b2846323d1c1d39a2001410ee84839bf37b7
Андрей Чертков <andreychertckov@yandex.ru>
2018-11-26 18:29:53 +0300

    Add query1

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ database_port = 5432
 database_user = 'postgres'
 database_password = 'car_sharing_password'
 database = 'car_sharing'
+start_stamp = 1
+end_stamp = 10
--- main.py
+++ main.py
@@ -21,15 +21,19 @@ def index():
     return 'Hello wold'
 
 
-@app.route('/query1', methods=['POST'])
+@app.route('/query1', methods=['GET'])
 def query1():
-    data = request.get_json()
-    if not 'username' in data:
+    if not 'username' in request.args:
         return make_error(400, 'Username field are required')
     
-    result_db = select1(data['username'])
+    result_db = select1(request.args.get('username'))
     print(result_db)
     return jsonify(result_db)
+
+
+@app.route('/query2', methods=['GET'])
+def query2():
+    pass
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
